cb280d472b47a70fb13877dfaa3e525263cb1451
Yang, Jonathan Junjie <jonathan.yang@merck.com>
2025-03-28 14:41:59 -0400

    streamline config services

--- src/orchestrators/assistant-orchestrator/orchestrator/jose.py
+++ src/orchestrators/assistant-orchestrator/orchestrator/jose.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routes import apis, websockets, deps
+config = get_config()
+from .deps import get_config
 
 
 # Instance of FastAPI app
@@ -8,6 +10,6 @@ app = FastAPI()
 # Initialize the app components
 deps.initialize()
 # API router to handle standard API routes
-app.include_router(apis.router)
+app.include_router(apis.router, prefix=f"/{config.service_name}/{str(config.version)}")
 # WebSocket router for handling WebSocket connections
-app.include_router(websockets.router)
+app.include_router(websockets.router, prefix=f"/{config.service_name}/{str(config.version)}")
