16b05eea27485e6868eff500ffcb38970aaf44e7
sdsmt05 <44756335+sdsmt05@users.noreply.github.com>
2025-05-07 10:04:16 -0500

    Feature/testing chat completion builder (#65)
    * Add testing for chat_completion_builder.py
    
    * Remove pytest-mock package
    
    * Merge uv.lock update
    
    ---------
    
    Co-authored-by: Brandon Zander <brandon.zander@merck.com>

---
+++ src/sk-agents/tests/custom/mock_chat_completion_factory.py
@@ -0,0 +1,51 @@
+from ska_utils import AppConfig, Config as UtilConfig
+
+from src.sk_agents.ska_types import ChatCompletionFactory, ModelType
+
+
+class MockChatCompletionFactory(ChatCompletionFactory):
+    _OPENAI_MODELS: list[str] = [
+        "gpt-model"
+    ]
+    _ANTHROPIC_MODELS: list[str] = [
+        "claude-model",
+    ]
+
+    TA_BASE_URL = UtilConfig(
+        env_name="TA_BASE_URL",
+        is_required=False,
+        default_value="https://<Your Azure OpenAI Service Endpoint>",
+    )
+    TA_API_VERSION = UtilConfig(
+        env_name="TA_API_VERSION", is_required=False, default_value="Some-Value"
+    )
+
+    _CONFIGS: list[UtilConfig] = [TA_BASE_URL, TA_API_VERSION]
+
+    def __init__(self, app_config: AppConfig):
+        pass
+
+    def get_configs():
+        return MockChatCompletionFactory._CONFIGS
+
+    def get_chat_completion_for_model_name(self, model_name: str, service_id: str):
+        if model_name in MockChatCompletionFactory._OPENAI_MODELS:
+            return {
+                "service_id": service_id,
+                "ai_model_id": model_name
+            }
+        raise ValueError("Model type not supported")
+
+    def get_model_type_for_name(self, model_name: str) -> ModelType:
+        if model_name in MockChatCompletionFactory._OPENAI_MODELS:
+            return "openai"
+        else:
+            raise ValueError(f"Unknown model name {model_name}")
+
+    def model_supports_structured_output(self, model_name: str) -> bool:
+        if model_name in MockChatCompletionFactory._OPENAI_MODELS:
+            return True
+        elif model_name in MockChatCompletionFactory._ANTHROPIC_MODELS:
+            return False
+        else:
+            raise ValueError(f"Unknown model name {model_name}")
---
+++ src/sk-agents/tests/test_chat_completion_builder.py
@@ -0,0 +1,89 @@
+import os
+
+import pytest
+from ska_utils import AppConfig
+
+from src.sk_agents.configs import configs
+from src.sk_agents.skagents.chat_completion_builder import ChatCompletionBuilder
+
+
+@pytest.fixture()
+def setup_env_fixture():
+    os.environ["TA_CUSTOM_CHAT_COMPLETION_FACTORY_MODULE"] = (
+        "tests/custom/mock_chat_completion_factory.py"
+    )
+    os.environ["TA_CUSTOM_CHAT_COMPLETION_FACTORY_CLASS_NAME"] = "MockChatCompletionFactory"
+
+
+@pytest.fixture()
+def app_config_fixture():
+    AppConfig.add_configs(configs)
+    app_config = AppConfig()
+    return app_config
+
+
+def test_init_with_no_module(app_config_fixture, setup_env_fixture):
+    os.environ.pop("TA_CUSTOM_CHAT_COMPLETION_FACTORY_MODULE", None)
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    assert isinstance(chat_completion_builder, ChatCompletionBuilder)
+
+
+def test_init_with_custom_module(app_config_fixture, setup_env_fixture):
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    assert isinstance(chat_completion_builder, ChatCompletionBuilder)
+
+
+def test_init_custom_chat_completion_class_not_found(app_config_fixture, setup_env_fixture):
+    os.environ.pop("TA_CUSTOM_CHAT_COMPLETION_FACTORY_CLASS_NAME", None)
+    AppConfig.add_configs(configs)
+    with pytest.raises(ValueError) as excinfo:
+        ChatCompletionBuilder(app_config_fixture)
+    assert str(excinfo.value) == "Custom Chat Completion Factory class name not provided"
+
+
+def test_init_custom_chat_completion_invalid_class(app_config_fixture, setup_env_fixture):
+    os.environ["TA_CUSTOM_CHAT_COMPLETION_FACTORY_CLASS_NAME"] = "InvalidClassName"
+    AppConfig.add_configs(configs)
+    with pytest.raises(ValueError) as excinfo:
+        ChatCompletionBuilder(app_config_fixture)
+    assert (
+        str(excinfo.value) == f"Custom Chat Completion Factory class: "
+        f"{os.environ['TA_CUSTOM_CHAT_COMPLETION_FACTORY_CLASS_NAME']}"
+        f"Not found in module: {os.environ['TA_CUSTOM_CHAT_COMPLETION_FACTORY_MODULE']}"
+    )
+
+
+def test_get_chat_completion_for_model(setup_env_fixture, app_config_fixture):
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    chat_completion = chat_completion_builder.get_chat_completion_for_model("test-id", "gpt-model")
+    assert chat_completion == {"service_id": "test-id", "ai_model_id": "gpt-model"}
+
+
+def test_get_chat_completion_for_model_error(app_config_fixture, setup_env_fixture):
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    with pytest.raises(ValueError):
+        chat_completion_builder.get_chat_completion_for_model("test-id", "unsuppored-model")
+
+
+def test_get_model_type_for_name(app_config_fixture, setup_env_fixture):
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    model_type = chat_completion_builder.get_model_type_for_name("gpt-model")
+    assert model_type == "openai"
+
+
+def test_get_model_type_for_name_error(app_config_fixture, setup_env_fixture):
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    with pytest.raises(ValueError):
+        chat_completion_builder.get_model_type_for_name("unsupported-model")
+
+
+def test_get_model_supports_structured_output(app_config_fixture, setup_env_fixture):
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    does_support = chat_completion_builder.model_supports_structured_output(model_name="gpt-model")
+    assert does_support is True
+
+
+def test_get_model_supports_structured_output_error(app_config_fixture, setup_env_fixture):
+    chat_completion_builder = ChatCompletionBuilder(app_config_fixture)
+    with pytest.raises(ValueError):
+        chat_completion_builder.model_supports_structured_output("unsupported-model")
