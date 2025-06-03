10a24675c305d70c9ea2b5c56888e5aaf4613013
Jose Garcia <jose.garcia6@merck.com>
2025-06-03 16:33:43 -0400

    test fall back agent and lint format

--- src/orchestrators/assistant-orchestrator/orchestrator/tests/test_agents.py
+++ src/orchestrators/assistant-orchestrator/orchestrator/tests/test_agents.py
@@ -1,9 +1,19 @@
-import pytest
 import json
-from agents import BaseAgent, AgentCatalog, FallbackAgent, AgentInput, ChatHistoryItem, FallbackInput
-from pydantic import BaseModel
-from ..model import Conversation
 
+import pytest
+from pydantic import BaseModel
+
+from agents import (
+    AgentCatalog,
+    AgentInput,
+    BaseAgent,
+    ChatHistoryItem,
+    FallbackAgent,
+    FallbackInput,
+    PromptAgent,
+)
+
+from ..model import Conversation
 
 
 class _AgentInput(BaseModel):
@@ -19,43 +29,50 @@ class MockAgent(BaseAgent):
 def empty_agent_catalog():
     return AgentCatalog(agents={})
 
+
 @pytest.fixture
 def single_agent_catalog():
-    return AgentCatalog(agents={
-        "TestAgent": MockAgent(
-            name='test agent',
-            description='agent used for unit test',
-            endpoint='ws://test_stream_endpoint',
-            endpoint_api='http://test_api_endpoint',
-            api_key='test_key'
-        )
-    })
+    return AgentCatalog(
+        agents={
+            "TestAgent": MockAgent(
+                name="test agent",
+                description="agent used for unit test",
+                endpoint="ws://test_stream_endpoint",
+                endpoint_api="http://test_api_endpoint",
+                api_key="test_key",
+            )
+        }
+    )
+
 
 @pytest.fixture
 def multiple_agent_catalog():
-    return AgentCatalog(agents={
-        "TestMathAgent": MockAgent(
-            name='Math Agent',
-            description='Math Agent used for unit test',
-            endpoint='ws://test_stream_endpoint',
-            endpoint_api='http://test_api_endpoint',
-            api_key='test_key'
-        ),
-        "TestWeatherAgent": MockAgent(
-            name='Weather Agent',
-            description='Weather Agent used for unit test',
-            endpoint='ws://test_stream_endpoint',
-            endpoint_api='http://test_api_endpoint',
-            api_key='test_key'
-        ),
-        "TestNewsAgent": MockAgent(
-            name='News Agent',
-            description='News Agent used for unit test',
-            endpoint='ws://test_stream_endpoint',
-            endpoint_api='http://test_api_endpoint',
-            api_key='test_key'
-        )
-    })
+    return AgentCatalog(
+        agents={
+            "TestMathAgent": MockAgent(
+                name="Math Agent",
+                description="Math Agent used for unit test",
+                endpoint="ws://test_stream_endpoint",
+                endpoint_api="http://test_api_endpoint",
+                api_key="test_key",
+            ),
+            "TestWeatherAgent": MockAgent(
+                name="Weather Agent",
+                description="Weather Agent used for unit test",
+                endpoint="ws://test_stream_endpoint",
+                endpoint_api="http://test_api_endpoint",
+                api_key="test_key",
+            ),
+            "TestNewsAgent": MockAgent(
+                name="News Agent",
+                description="News Agent used for unit test",
+                endpoint="ws://test_stream_endpoint",
+                endpoint_api="http://test_api_endpoint",
+                api_key="test_key",
+            ),
+        }
+    )
+
 
 @pytest.fixture
 def fallback_agent_base_params():
@@ -64,28 +81,26 @@ def fallback_agent_base_params():
         "description": "A fallback agent",
         "endpoint": "ws://fallback",
         "endpoint_api": "http://fallback_api",
-        "api_key": "fallback_key"
+        "api_key": "fallback_key",
     }
 
-@pytest.fixture        
+
+@pytest.fixture
 def agent_instance():
     agent = MockAgent(
-        name='test agent',
-        description='agent used for unit test',
-        endpoint='ws://test_stream_endpoint',
-        endpoint_api='http://test_api_endpoint',
-        api_key='test_key'
+        name="test agent",
+        description="agent used for unit test",
+        endpoint="ws://test_stream_endpoint",
+        endpoint_api="http://test_api_endpoint",
+        api_key="test_key",
     )
     return agent
 
-@pytest.fixture        
+
+@pytest.fixture
 def conversation_for_testing():
-    return Conversation(
-        conversation_id= 'test-id',
-        user_id= 'test-iser',
-        history= [],
-        user_context= {}
-    )
+    return Conversation(conversation_id="test-id", user_id="test-iser", history=[], user_context={})
+
 
 def test_agent_get_invoke_input(agent_instance):
     agent_input = _AgentInput(data="Test Hello World")
@@ -93,71 +108,70 @@ def test_agent_get_invoke_input(agent_instance):
 
     assert response == "Processed input: Test Hello World"
 
+
 def test_invoke_api_success(mocker, agent_instance, conversation_for_testing):
-    _conversation_to_agent_input = mocker.patch("agents._conversation_to_agent_input", return_value=_AgentInput(data="mocked input"))
+    _conversation_to_agent_input = mocker.patch(
+        "agents._conversation_to_agent_input", return_value=_AgentInput(data="mocked input")
+    )
 
     # Mock requests.post
     mock_response = mocker.Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"status": "success", "data": "response_from_api"}
-    post_request = mocker.patch('requests.post', return_value=mock_response)
+    post_request = mocker.patch("requests.post", return_value=mock_response)
 
     response = agent_instance.invoke_api(conversation_for_testing, authorization="xyz")
-    
+
     _conversation_to_agent_input.assert_called_once_with(conversation_for_testing)
 
     expected_headers = {
         "taAgwKey": agent_instance.api_key,
         "Authorization": "xyz",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     post_request.assert_called_once_with(
-            agent_instance.endpoint_api,
-            data="Processed input: mocked input",
-            headers=expected_headers
-        )
-    
+        agent_instance.endpoint_api, data="Processed input: mocked input", headers=expected_headers
+    )
+
     assert response == {"status": "success", "data": "response_from_api"}
 
 
 def test_invoke_api_non_200_status(mocker, agent_instance, conversation_for_testing):
-    _conversation_to_agent_input = mocker.patch("agents._conversation_to_agent_input", return_value=_AgentInput(data="mocked input"))
-    
+    mocker.patch(
+        "agents._conversation_to_agent_input", return_value=_AgentInput(data="mocked input")
+    )
+
     mock_response = mocker.Mock()
     mock_response.status_code = 404
     mock_response.text = "Not Found"
-    post_request = mocker.patch('requests.post', return_value=mock_response)
+    mocker.patch("requests.post", return_value=mock_response)
 
-    with pytest.raises(Exception) as excinfo:
+    with pytest.raises(Exception):
         agent_instance.invoke_api(conversation_for_testing)
-    
-    
+
+
 def test_multiple_agents_catalog(fallback_agent_base_params, multiple_agent_catalog):
     agent_input = AgentInput(
-        chat_history=[
-            ChatHistoryItem(
-                role="user",
-                content="hi this is a test"
-            )
-        ],
-        user_context={}
+        chat_history=[ChatHistoryItem(role="user", content="hi this is a test")], user_context={}
     )
 
     fallback_agent = FallbackAgent(
-        agent_catalog = multiple_agent_catalog,
-        **fallback_agent_base_params
+        agent_catalog=multiple_agent_catalog, **fallback_agent_base_params
     )
 
     response = fallback_agent.get_invoke_input(agent_input)
-    
+
     result_data = json.loads(response)
-    
+
     expected_result = FallbackInput(
         chat_history=agent_input.chat_history,
         user_context=agent_input.user_context,
-        agents=multiple_agent_catalog.agents
+        agents=[
+            PromptAgent(name=value.name, description=value.description)
+            for value in multiple_agent_catalog.agents.values()
+        ],
     )
-    expected_result = expected_result.model_dump(mode='json')
-    print('***Expected result: ',expected_result)
+    expected_result = expected_result.model_dump(mode="json")
+
     assert result_data == expected_result
