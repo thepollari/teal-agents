88e2dbaa67c5192895330dc424ad97dde11dcaae
Jose Garcia <54993787+jgarcia2411@users.noreply.github.com>
2025-06-17 15:16:13 -0400

    async implementation in chat_history_manager (#85)
    Co-authored-by: Jose Garcia <jose.garcia6@merck.com>

--- src/orchestrators/assistant-orchestrator/services/data/conversation_manager.py
+++ src/orchestrators/assistant-orchestrator/services/data/conversation_manager.py
@@ -31,17 +31,17 @@ class ConversationManager:
     def __init__(self, chat_history_manager: ChatHistoryManager):
         self.chat_history_manager = chat_history_manager
 
-    def new_conversation(
+    async def new_conversation(
         self, orchestrator_name: str, user_id: str, is_resumed: bool
     ) -> ConversationResponse:
-        return self._new_conversation(orchestrator_name, user_id, is_resumed)
+        return await self._new_conversation(orchestrator_name, user_id, is_resumed)
 
-    def get_conversation(
+    async def get_conversation(
         self, orchestrator_name: str, user_id: str, conversation_id: str
     ) -> ConversationResponse:
-        return self._get_conversation(orchestrator_name, user_id, conversation_id)
+        return await self._get_conversation(orchestrator_name, user_id, conversation_id)
 
-    def add_conversation_message(
+    async def add_conversation_message(
         self,
         orchestrator_name: str,
         conversation_id: str,
@@ -49,7 +49,7 @@ class ConversationManager:
         agent_name: str,
         message: str,
     ) -> GeneralResponse:
-        self.chat_history_manager.add_session_item(
+        await self.chat_history_manager.add_session_item(
             orchestrator_name=orchestrator_name,
             session_id=conversation_id,
             item=ChatHistoryItem(
@@ -61,7 +61,7 @@ class ConversationManager:
         )
         return GeneralResponse(status=200, message="Message added successfully")
 
-    def _get_conversation(
+    async def _get_conversation(
         self, orchestrator_name: str, user_id: str, session_id: str
     ) -> ConversationResponse:
         st = get_telemetry()
@@ -70,10 +70,10 @@ class ConversationManager:
             if st.telemetry_enabled()
             else nullcontext()
         ):
-            messages = self._load_messages(orchestrator_name, user_id, session_id)
+            messages = await self._load_messages(orchestrator_name, user_id, session_id)
         return ConversationResponse(conversation_id=session_id, history=messages)
 
-    def _new_conversation(
+    async def _new_conversation(
         self, orchestrator_name: str, user_id: str, is_resumed: bool
     ) -> ConversationResponse:
         st = get_telemetry()
@@ -92,16 +92,16 @@ class ConversationManager:
         if is_resumed:
             # Retrieve previous session ID
             with create_span("retrieve-last-session-id"):
-                previous_session = self._get_last_chat_history_id(orchestrator_name, user_id)
+                previous_session = await self._get_last_chat_history_id(orchestrator_name, user_id)
 
             with create_span("retrieve-session-history"):
-                messages = self._load_messages(orchestrator_name, user_id, previous_session)
+                messages = await self._load_messages(orchestrator_name, user_id, previous_session)
         else:
             previous_session = None
             messages = []
 
         with create_span("add-chat-history-session"):
-            self.chat_history_manager.add_chat_history_session(
+            await self.chat_history_manager.add_chat_history_session(
                 orchestrator_name,
                 ChatHistory(
                     user_id=user_id,
@@ -111,17 +111,17 @@ class ConversationManager:
                 ),
             )
         with create_span("update-last-session-id"):
-            self.chat_history_manager.set_last_session_id_for_user(
+            await self.chat_history_manager.set_last_session_id_for_user(
                 orchestrator_name, user_id, session_id
             )
         return ConversationResponse(conversation_id=session_id, history=messages)
 
-    def _load_messages(
+    async def _load_messages(
         self, orchestrator_name: str, user_id: str, previous_session: str | None
     ) -> list[Union["UserMessage", "AgentMessage"]]:
         messages: list[UserMessage | AgentMessage] = []
         if previous_session:
-            histories: list[list[ChatHistoryItem]] = self._load_chat_history(
+            histories: list[list[ChatHistoryItem]] = await self._load_chat_history(
                 orchestrator_name, user_id, previous_session
             )
 
@@ -138,17 +138,19 @@ class ConversationManager:
                 messages.append(_chat_history_item_to_message(item))
         return messages
 
-    def _get_last_chat_history_id(self, orchestrator_name: str, user_id: str) -> str | None:
-        return self.chat_history_manager.get_last_session_id_for_user(orchestrator_name, user_id)
+    async def _get_last_chat_history_id(self, orchestrator_name: str, user_id: str) -> str | None:
+        return await self.chat_history_manager.get_last_session_id_for_user(
+            orchestrator_name, user_id
+        )
 
-    def _load_chat_history(
+    async def _load_chat_history(
         self, orchestrator_name: str, user_id: str, session_id: str
     ) -> list[ChatHistory]:
-        session_chat_history = self._load_chat_history_from_persistence(
+        session_chat_history = await self._load_chat_history_from_persistence(
             orchestrator_name, user_id, session_id
         )
         if session_chat_history.previous_session:
-            previous_histories = self._load_chat_history(
+            previous_histories = await self._load_chat_history(
                 orchestrator_name, user_id, session_chat_history.previous_session
             )
             previous_histories.append(session_chat_history)
@@ -156,9 +158,9 @@ class ConversationManager:
         else:
             return [session_chat_history]
 
-    def _load_chat_history_from_persistence(
+    async def _load_chat_history_from_persistence(
         self, orchestrator_name: str, user_id: str, session_id: str
     ) -> ChatHistory:
-        return self.chat_history_manager.get_chat_history_session(
+        return await self.chat_history_manager.get_chat_history_session(
             orchestrator_name, user_id, session_id
         )
