363c91f2a938a9a0447db05a0da6bab4849b9638
David Daniel <david.daniel@merck.com>
2025-07-10 06:49:03 -0500

    Exclude healthcheck endpoint from traces

--- src/sk-agents/src/sk_agents/middleware/telemetry_middleware.py
+++ src/sk-agents/src/sk_agents/middleware/telemetry_middleware.py
@@ -4,23 +4,29 @@ from fastapi import FastAPI, Request, Response
 from opentelemetry.propagate import extract
 from ska_utils import Telemetry
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from typing import List
 
 
 class TelemetryMiddleware(BaseHTTPMiddleware):
-    _telemetry_excluded_paths: list[str] = ["/openapi.json"]
+    _telemetry_excluded_path_suffixes: List[str] = ["/openapi.json"]
 
     def __init__(self, app: FastAPI, st: Telemetry):
         super().__init__(app)
         self.st = st
 
-    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
         context = extract(request.headers)
         with (
             self.st.tracer.start_as_current_span(
                 f"{request.method} {request.url.path}", context=context
             )
             if self.st.telemetry_enabled()
-            and request.url.path not in self._telemetry_excluded_paths
+            and not any(
+                request.url.path.endswith(suffix)
+                for suffix in self._telemetry_excluded_path_suffixes
+            )
             else nullcontext()
         ):
             response = await call_next(request)
