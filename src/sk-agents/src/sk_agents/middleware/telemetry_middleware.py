ddd77eebabb488962ca3766a281ed520b4efeede
David Daniel <david.daniel@merck.com>
2025-07-10 06:51:18 -0500

    Format

--- src/sk-agents/src/sk_agents/middleware/telemetry_middleware.py
+++ src/sk-agents/src/sk_agents/middleware/telemetry_middleware.py
@@ -4,19 +4,16 @@ from fastapi import FastAPI, Request, Response
 from opentelemetry.propagate import extract
 from ska_utils import Telemetry
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
-from typing import List
 
 
 class TelemetryMiddleware(BaseHTTPMiddleware):
-    _telemetry_excluded_path_suffixes: List[str] = ["/openapi.json"]
+    _telemetry_excluded_path_suffixes: list[str] = ["/openapi.json"]
 
     def __init__(self, app: FastAPI, st: Telemetry):
         super().__init__(app)
         self.st = st
 
-    async def dispatch(
-        self, request: Request, call_next: RequestResponseEndpoint
-    ) -> Response:
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         context = extract(request.headers)
         with (
             self.st.tracer.start_as_current_span(
